6e92e4f109e22bb0a0cb6bce7ab1005bc0699c37
Emre Solak <s.emresolak@dreamharvesters.com>
2017-08-11 13:14:58 +0300

    MoveAssetsAction implementation

--- Assets/Editor/BuildSystem/BuildAction/MoveFolders.cs
+++ Assets/Editor/BuildSystem/BuildAction/MoveFolders.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEditor;
 using System.Collections;
 
 namespace BuildSystem
@@ -12,8 +13,52 @@ namespace BuildSystem
         [SerializeField]
         private string targetFolder;
 
+        [SerializeField]
+        private bool createTargetFolder;
+
+        public string[] FoldersToMove 
+        {
+            get { return foldersToMove; }
+            set { foldersToMove = value; }
+        }
+
+        public string TargetFolder
+        {
+            get { return targetFolder; }
+            set { targetFolder = value; }
+        }
+
+        public bool CreateTargetFolder
+        {
+            get { return createTargetFolder; }
+            set { createTargetFolder = value; }
+        }
+
         public bool Execute()
         {
+            if (!System.IO.Directory.Exists(targetFolder) && createTargetFolder)
+            {
+                string folderName, parentPath;
+                Utils.GetFolderAndParentPath(targetFolder, out parentPath, out folderName);
+
+                AssetDatabase.CreateFolder(parentPath, folderName);
+
+                AssetDatabase.Refresh();
+            }
+
+            for (int i = 0; i < foldersToMove.Length; i++)
+            {
+                try
+                {
+                    FileUtil.MoveFileOrDirectory(foldersToMove[i], targetFolder);
+                }
+                catch(System.Exception e)
+                {
+                    Debug.Log(e.Message);
+                    return false;
+                }
+            }
+
             return true;
         }
 	}
---
+++ Assets/Editor/BuildSystem/Utils.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+namespace BuildSystem
+{
+	public static class Utils
+	{
+		public static void GetFolderAndParentPath(string path, out string parentPath, out string folderName)
+        {
+            path = path.Replace("\\", "/");
+            parentPath = path.Substring(0, path.LastIndexOf("/"));
+            folderName = path.Substring(path.LastIndexOf("/") + 1);
+        }
+	}
+}
