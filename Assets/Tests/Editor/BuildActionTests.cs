dc3aded082b7258c66c52cd497b97f98698c83a5
Emre Solak <s.emresolak@dreamharvesters.com>
2017-08-11 13:22:10 +0300

    Added BuildActionTests

---
+++ Assets/Tests/Editor/BuildActionTests.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.TestTools;
+using UnityEditor;
+using NUnit.Framework;
+
+namespace BuildSystem
+{
+	public class BuildActionTests
+	{
+		[Test]
+        public void Test_MoveFolders_MoveWithoutTargetFolder_Pass()
+        {
+            AssetDatabase.CreateFolder("Assets", "TestMoveFolder");
+            AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<BuildConfiguration>(), "Assets/TestMoveFolder/BuildConfiguration.asset");
+
+            MoveFolders mf = ScriptableObject.CreateInstance<MoveFolders>();
+            mf.FoldersToMove = new string[] { "Assets/TestMoveFolder" };
+            mf.TargetFolder = "Assets/TestTargetFolder";
+            mf.CreateTargetFolder = true;
+
+            bool result = mf.Execute();
+
+			FileUtil.DeleteFileOrDirectory("Assets/TestMoveFolder");
+			FileUtil.DeleteFileOrDirectory("Assets/TestTargetFolder");
+
+            Assert.IsTrue(result);
+        }
+	}
+}
